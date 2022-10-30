921bd0db0b0b33e9da92b189cf63b2a2a60a23ee
hosein <hosein.maghoul@gmail.com>
2022-10-30 22:45:01 +0330

    The simple exercise of Rabbitmq

---
+++ python/4/info_receiver.py
@@ -0,0 +1,29 @@
+import pika
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+ch = connection.channel()
+
+ch.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+result = ch.queue_declare(queue='', exclusive=True)
+
+qname = result.method.queue
+
+severities = ('info', 'warning', 'error')
+
+for seventiy in severities:
+    ch.queue_bind(exchange='direct_logs', queue=qname, routing_key=seventiy)
+
+print('wating for messages')
+
+def callback(ch, method, properties, body):
+    print(f'{method.routing_key} , {body}')
+
+ch.basic_consume(queue=qname, on_message_callback=callback, auto_ack=True)
+
+ch.start_consuming()
+
+
+
